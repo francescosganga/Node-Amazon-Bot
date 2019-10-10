c831008a6509ec13ada27a4ba5cca46a72e90c3e
Francesco Sganga <sgangacreations@gmail.com>
2019-10-10 21:38:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cron = require('node-cron');
 
 console.log("[Node Amazon Bot]$: started");
  
-cron.schedule('*/10 * * * *', () => {
+cron.schedule('*/30 * * * *', () => {
 	amazonStuffs.updateBestSellers();
 });
 
