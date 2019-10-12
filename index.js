f61b8c9754a0eea2c431460002046730cc0668e8
Francesco Sganga <sgangacreations@gmail.com>
2019-10-12 15:27:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var amazonStuffs = require('./app/amazonStuffs');
 
 consoleLog.send("starting");
 
-var minutes = 2, the_interval = minutes * 60 * 1000;
+var minutes = 15, the_interval = minutes * 60 * 1000;
 setInterval(function() {
 	amazonStuffs.updateBestSellers();
 	amazonStuffs.bestSellersToTelegram();
